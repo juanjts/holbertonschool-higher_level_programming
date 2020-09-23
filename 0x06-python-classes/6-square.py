850eeb78fe865a45045547cf307eddd44ee130fd
juanjts <1843@holbertonschool.com>
2020-09-23 02:36:13 +0000

    go back

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,6 +8,11 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """documentation"""
 
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = size
             self.__position = position
 
