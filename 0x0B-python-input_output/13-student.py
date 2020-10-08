4edf9f6984b368efcabefc4d8c18de8d8b9e4823
juanjts <1843@holbertonschool.com>
2020-10-08 05:01:19 +0000

    task

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""module space"""
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize method"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            dic = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    dic[i] = getattr(self, i)
+            return dic
+        return self.__dict__
+    
+    def reload_from_json(self, json):
+        for i in json:
+            self.__dict__[i] = json[i]
