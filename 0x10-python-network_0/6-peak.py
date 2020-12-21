90399ba829eaa78e12c4916bca9f0c8ca023e478
juanjts <1843@holbertonschool.com>
2020-12-21 18:05:40 +0000

    task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Write a function that finds a peak in a list of unsorted integers"""
+""" Find peak in unsorted integers """
 
 
 def find_peak(list_of_integers):
-    """Prototype function to find peak of integers"""
+    """Prototype function used to find a peak of integers"""
 
-    if list_of_integers is None or list_of_integers == []:
+    if len(list_of_integers) == 0:
         return None
-    for i in range(0, len(list_of_integers)):
-        peak = list_of_integers[0]
-        if i == len(list_of_integers) - 1:
-            break
-        if list_of_integers[i] > list_of_integers[i + 1]\
-                and list_of_integers[i] > list_of_integers[i - 1]:
-            peak = list_of_integers[i]
-            break
-    return peak
+    argvs = list_of_integers
+    leng = len(list_of_integers)
+    peak = leng // 2
+    if (peak == leng - 1 or argvs[peak] >= argvs[peak + 1]):
+        if (peak == 0 or argvs[peak] >= argvs[peak - 1]):
+            return argvs[peak]
+    if peak != leng - 1 and argvs[peak + 1] > argvs[peak]:
+        return find_peak(argvs[peak + 1:])
+    return find_peak(argvs[:peak])
