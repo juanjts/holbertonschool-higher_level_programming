62d7c46e09b0f576c997deb1f1ac2a3012da1503
juanjts <1843@holbertonschool.com>
2021-02-08 19:32:17 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const Squares = require('./5-square')
+const Squares = require('./5-square');
 
-class Square extends Squares{
-    charPrint(c) {
-        if (c === undefined){
-            c = 'X'
-        }
-        for (let x = 0; x < this.height; x++){
-            console.log(c.repeat(this.width))
-        }
+class Square extends Squares {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let x = 0; x < this.height; x++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-exports.esrever = function (list){
-    let x = 0;
-    let reverse = []
-    for (let i = list.length; i > 0; i--){
-        reverse[x] = list[i - 1];
-        x++;
-    }
-    return reverse;
-}
+exports.esrever = function (list) {
+  let x = 0;
+  const reverse = [];
+  for (let i = list.length; i > 0; i--) {
+    reverse[x] = list[i - 1];
+    x++;
+  }
+  return reverse;
+};
